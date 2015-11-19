8f1e54dd2b77dde76ef6a457dfacda914174e044
Ali Shahid <Ali Shahid>
2015-11-19 14:37:07 -0500

    all but content

---
+++ Source/MyProject2/PlayerHealth.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/HUD.h"
+#include "PlayerHealth.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT2_API APlayerHealth : public AHUD
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
